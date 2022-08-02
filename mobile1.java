a5a9efe639b5dee643db43045e5476782aa13593
HarshithaA2998 <110400328+HarshithaA2998@users.noreply.github.com>
2022-08-02 11:33:32 +0530

    Add files via upload

--- mobile1.java
+++ mobile1.java
@@ -19,6 +19,8 @@ public class mobile1 {
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.findElement(By.xpath("//a[@href='signup.html']")).click();
+		driver.findElement(By.xpath("//input[@placeholder='First Name']")).sendKeys("Harshitha");
+		driver.findElement(By.xpath("//input[@placeholder='Last Name']")).sendKeys("A");
 		
 	}
 
